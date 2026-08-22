15c81dd4d71379f70a05f2302e308a15e8be57a6
lingonsoda <iveros.markus@gmail.com>
2026-08-22 10:57:45 +0200

    added llm function calls to main

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
-from dotenv import load_dotenv
-from openai import OpenAI
+from dotenv import load_dotenv # pyright: ignore[reportMissingImports]
+from openai import OpenAI # pyright: ignore[reportMissingImports]
 import argparse
+from prompts import system_prompt
+from functions.call_function import available_functions
+import json
 
 def main():
     print("Hello from aiagent!")
@@ -22,22 +25,30 @@ def main():
     args = parser.parse_args()
 
     messages = [
+        {"role": "system", "content": system_prompt},
         {"role": "user", "content": args.user_prompt},
     ]
 
     response = client.chat.completions.create(
         model = "openrouter/free",
-        messages = messages
+        messages = messages,
+        tools = available_functions,
     )
     
-    if args.verbose:
-        if response.usage == None:
-            raise RuntimeError("No usage data found in response")
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage.prompt_tokens}")
-        print(f"Response tokens: {response.usage.completion_tokens}")
+    #if args.verbose:
+        #if response.usage == None:
+            #raise RuntimeError("No usage data found in response")
+        #print(f"User prompt: {args.user_prompt}")
+        #print(f"Prompt tokens: {response.usage.prompt_tokens}")
+        #print(f"Response tokens: {response.usage.completion_tokens}")
 
-    print(response.choices[0].message.content)
+    #print(response.choices[0].message.content)
+
+    message = response.choices[0].message
+    if message.tool_calls:
+        for tool_call in message.tool_calls:
+            function_args = json.loads(tool_call.function.arguments or "{}")
+            print(f"Calling function: {tool_call.function.name}({function_args}")
 
 
 if __name__ == "__main__":
